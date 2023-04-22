ea853eeb75d2050d37a0ed3050e296d5f436a487
amralaayassen <amralaayassen22@gmail.com>
2023-04-22 16:44:19 +0200

    add docker

---
+++ scripts/build-image.sh
@@ -0,0 +1,28 @@
+#
+# Builds a Docker image.
+#
+# Environment variables:
+#
+#   CONTAINER_REGISTRY - The hostname of your container registry.
+#   VERSION - The version number to tag the images with.
+#
+# Parameters:
+#
+#   1 - The path to the code for the image and the name of the image.
+#
+# Usage:
+#
+#       ./scripts/build-image.sh <image-name>
+#
+# Example command line usage:
+#
+#       ./scripts/build-image.sh service
+#
+
+set -u # or set -o nounset
+: "$1"
+: "$CONTAINER_REGISTRY"
+: "$VERSION"
+
+export DIR=$1
+docker build -t $CONTAINER_REGISTRY/$DIR:$VERSION --file ./$DIR/Dockerfile ./$DIR
