c5b024fb25645c45eababdb698824180dc31647f
amralaayassen <amralaayassen22@gmail.com>
2023-04-23 20:06:26 +0200

    install kubectl

--- scripts/create-user.sh
+++ scripts/create-user.sh
@@ -9,12 +9,7 @@
 
 # Exit immediately if a command exits with a non-zero status.
 
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
-chmod +x kubectl
-mkdir -p ~/.local/bin
-mv ./kubectl ~/.local/bin/kubectl
-kubectl version --client
+
 set -e
 
 function check_command() {
